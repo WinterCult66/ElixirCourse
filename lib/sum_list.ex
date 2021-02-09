091d785775aa0b9e64168f8fd67456eb2c8162c9
Kevin Alexander Rodriguez Rodriguez <kerodriguez@personalsoft.com.co>
2021-02-09 17:05:05 -0500

    create repository and exercices

---
+++ lib/sum_list.ex
@@ -0,0 +1,14 @@
+defmodule SumList do
+
+
+  @listNomina [52.8000,41.255,96.633];
+
+  def sumList([first|tail] \\  @listNomina, suma \\0) do
+    result = suma + first
+    sumList(tail,result)
+    IO.inspect(" #{result}"  )
+  end
+
+  def sumList([],suma), do: suma
+
+end
